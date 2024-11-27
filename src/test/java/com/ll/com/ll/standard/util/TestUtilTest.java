73bfd683bf3a82d2371f31875206e0bcf4ecf2c6
pcyscott <pcyscott@gmail.com>
2024-11-27 15:29:47 +0900

    f1

---
+++ src/test/java/com/ll/com/ll/standard/util/TestUtilTest.java
@@ -0,0 +1,30 @@
+package com.ll.com.ll.standard.util;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestUtilTest {
+    @Test
+    @DisplayName("IOUtil.genScanner()")
+    public void t1(){
+       Scanner scanner =  TestUtil.getScanner(
+                """
+                        등록
+                        나의 죽음을 적에게 알리지 마라
+                        이순신
+                        """.stripIndent());
+        String cmd = scanner.nextLine();
+        String content = scanner.nextLine();
+        String author = scanner.nextLine();
+
+        assertThat(cmd).isEqualTo("등록");
+        assertThat(content).isEqualTo("나의 죽음을 적에게 알리지 마라");
+        assertThat(author).isEqualTo("이순신");
+    }
+
+
+}

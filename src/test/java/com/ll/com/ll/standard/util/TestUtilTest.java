73f7b136ddd5d2846805a4cac27898d49b4ea521
pcyscott <pcyscott@gmail.com>
2024-11-27 16:37:57 +0900

    r1

--- src/test/java/com/ll/com/ll/standard/util/TestUtilTest.java
+++ src/test/java/com/ll/com/ll/standard/util/TestUtilTest.java
@@ -3,6 +3,7 @@ package com.ll.com.ll.standard.util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,5 +27,17 @@ public class TestUtilTest {
         assertThat(author).isEqualTo("이순신");
     }
 
+    @Test
+    @DisplayName("IOUtil.setOutToByteArray()")
+    public void t2(){
+       ByteArrayOutputStream byteArrayOutputStream =  TestUtil.setOutToByteArray();
 
+       System.out.println("2 / 나의 죽음을 적에게 알리지 마라 / 이순신");
+
+       String out = byteArrayOutputStream.toString().trim();
+       TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+
+       assertThat(out).isEqualTo("2 / 나의 죽음을 적에게 알리지 마라 / 이순신");
+       System.out.println("이제는 화면에 출력됩니다.");
+    }
 }

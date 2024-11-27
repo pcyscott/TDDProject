5ff8f25f2f5b165513d20182dd8db639925da865
pcyscott <pcyscott@gmail.com>
2024-11-28 01:17:22 +0900

    r3

--- src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
+++ src/test/java/com/ll/domain/wiseSaying/controller/WiseSayingControllerTest.java
@@ -28,4 +28,25 @@ public class WiseSayingControllerTest {
         assertThat(output.toString())
                 .contains("== 명언 앱 ==");
     }
+
+    @Test
+    @DisplayName("명령) ")
+    public void t2(){
+        Scanner scanner =TestUtil.getScanner("""
+                목록
+                종료
+                """);
+        ByteArrayOutputStream outputStream =TestUtil.setOutToByteArray();
+
+
+        App app = new App(scanner);
+        app.run();
+
+        String output = outputStream.toString();
+
+        TestUtil.clearSetOutToByteArray(outputStream);
+
+        assertThat(output.toString())
+                .contains("명령) ");
+    }
 }
